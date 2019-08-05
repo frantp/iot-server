2c3b670bb66aeb790cb3ae20e12fc85d8adcd5e1
Francisco Troncoso <ftroncoso@uvigo.es>
2019-08-06 00:24:42 +0200

    Removed unnecessary file

--- init_cfg.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker run --rm telegraf telegraf config > etc/telegraf.conf
-docker run --rm influxdb influxd config > etc/influxdb.conf
-docker run --rm kapacitor kapacitord config > etc/kapacitor.conf
